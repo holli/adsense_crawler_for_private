b39e9c20272d07a0170fc170dfebbe67dc28727b
Olli Huotari <olli.huotari@iki.fi>
2011-12-26 20:23:02 +0200

    some comment updates

--- app/controllers/adsense_crawler_for_private/adsense_crawler_login_controller.rb
+++ app/controllers/adsense_crawler_for_private/adsense_crawler_login_controller.rb
@@ -1,9 +1,6 @@
 module AdsenseCrawlerForPrivate
   class AdsenseCrawlerLoginController < ApplicationController
 
-    CRAWLER_AD_COOKIE = "joo"
-    WES_COOKIE_DOMAIN = "joo" # Set cookie domain automatically with two dots .adsf.inv
-
     def login
       unless AdsenseCrawlerForPrivate.crawler_password.blank?
 
@@ -13,7 +10,6 @@ module AdsenseCrawlerForPrivate
         if (AdsenseCrawlerForPrivate.ip_check(request) and
             crawler_name == AdsenseCrawlerForPrivate.crawler_name and
             crawler_password == AdsenseCrawlerForPrivate.crawler_password)
-          #if (AdsenseCrawlerFilter.crawler_ad_ip_check(request) && crawler_name == CRAWLER_AD_NAME && crawler_password == CRAWLER_AD_PASSWORD)
 
           cookies.signed[AdsenseCrawlerForPrivate.cookie_name] = {
               :value => AdsenseCrawlerForPrivate.cookie_str(crawler_name, request),
