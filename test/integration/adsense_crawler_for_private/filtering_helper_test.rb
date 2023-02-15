bce65893e0ebb4baff3888668ce7064d40331814
Olli Huotari <olli.huotari@iki.fi>
2023-02-15 17:09:15 +0200

    Fixed tests to pass on rails 6 and 7

--- test/integration/adsense_crawler_for_private/filtering_helper_test.rb
+++ test/integration/adsense_crawler_for_private/filtering_helper_test.rb
@@ -10,6 +10,7 @@ class FilteringHelperTest < ActionDispatch::IntegrationTest
 
     @crawler_name = AdsenseCrawlerForPrivate.crawler_name = "craw_name_TEST"
     @crawler_password = AdsenseCrawlerForPrivate.crawler_password = "crawler_password_TEST"
+    binding.pry
   end
 
   test "normal_render as normal for testing dummy application itself" do
@@ -49,17 +50,13 @@ class FilteringHelperTest < ActionDispatch::IntegrationTest
   # INVALID COOKIES TEST
 
   test "specific render for expired cookie" do
-    cookies[AdsenseCrawlerForPrivate.cookie_name] =
-        ActiveSupport::MessageVerifier.new(Dummy::Application.config.secret_token).generate(
-            [@crawler_name, @crawler_password, 2.days.ago.httpdate, "127.0.0.1"].to_json)
+    set_login_cookie(nil, nil, nil, 2.days.ago)
     get '/forbidden_render'
     assert_response :redirect, "should not render private page"
   end
 
   test "specific render for not allowed crawler_name" do
-    cookies[AdsenseCrawlerForPrivate.cookie_name] =
-        ActiveSupport::MessageVerifier.new(Dummy::Application.config.secret_token).generate(
-            ["not_allowed_crawler", @crawler_password, 2.days.from_now.httpdate, "127.0.0.1"].to_json)
+    set_login_cookie('not_allowed_crawler', nil, nil, nil)
     get '/forbidden_render'
     assert_response :redirect, "should not render private page"
   end
@@ -76,14 +73,16 @@ class FilteringHelperTest < ActionDispatch::IntegrationTest
   # HELPERS
 
   # This is basically setting a signed cookie
-  def set_login_cookie(crawler_name=nil, crawler_password=nil, ip=nil)
+  def set_login_cookie(crawler_name=nil, crawler_password=nil, ip=nil, date=nil)
     crawler_name ||= @crawler_name
     crawler_password ||= @crawler_password
     ip ||= "127.0.0.1"
+    date ||= 2.days.from_now
 
     cookie_jar = ActionDispatch::Request.new(Rails.application.env_config.deep_dup).cookie_jar
     cookie_jar.signed[AdsenseCrawlerForPrivate.cookie_name] =
-      AdsenseCrawlerForPrivate.cookie_str(crawler_name, crawler_password, 2.days.from_now, ip)
+      AdsenseCrawlerForPrivate.cookie_str(crawler_name, crawler_password, date, ip)
+
     cookies[AdsenseCrawlerForPrivate.cookie_name] = cookie_jar[AdsenseCrawlerForPrivate.cookie_name]
   end
 
